2653150aef847a7ef4ddef789c1b23ccfdfc1899
AnarchoFerret <70930754+anarchoferret@users.noreply.github.com>
2021-08-06 18:15:47 -0500

    Update update.sh
    Added the ability to see the updates as well as some fixes with flatpak.

--- update.sh
+++ update.sh
@@ -1,26 +1,45 @@
 #!/bin/sh
 echo "Checking for new software..."
-echo " "
 
 # update apt repos
 echo " "
-echo "Checking if Debian..."
 #[ -d "/usr/lib/apt" ] && echo "APT exists"
 if [ -d "/usr/lib/apt" ]
 then
   echo "This system is using Debian!"
   echo "Checking for updated apt packages..."
-  sudo apt update > log.txt
+  sudo apt update > log.txt 2>/dev/null
   statement="$(grep "All packages are up to date."  log.txt)"
 
 # upgrade existing packages
   echo " "
+  echo "$statement"
   if [ "$statement" = "All packages are up to date." ] # Note:  quotes are needed to denote a string
   then
     echo "No updates needed through apt."
   else
-    echo "APT has updates; downloading and installing..."
-      sudo apt upgrade -y
+    echo "APT has updates:  "
+    read -p "Would you like to update these packages? [Y/N]:  " answer1
+    if [ "$answer1" = "Y"]
+    then
+      apt list --upgradable
+    elif [ "$answer1" = "N" ]
+    then
+      echo "Upgrades hidden."
+    else
+      echo "User input not understood.  Defaulting to 'No' to save space."
+    fi
+
+    read -p "Would you like to update these packages? [Y/N]:  " answer2
+    if [ "$answer2" = "Y" ]
+    then
+      sudo apt-get upgrade -y > /dev/null
+    elif [ "$answer2" = "N" ]
+    then
+      echo "Update Canceled"
+    else
+      echo "User input not understood.  Assuming no updates wanted."
+    fi
   fi
   echo "Update Complete!"
   rm log.txt 
@@ -44,9 +63,6 @@ then
   fi
   rm pop-check.txt 
 fi
-
-# pop-upgrade recovery upgrade from-release
-echo " "
 echo "Update Complete!"
 
 # Update Flatpak packages
@@ -54,9 +70,16 @@ if [ -d "/usr/share/flatpak" ]
 then
   echo " "
   echo "Checking for Flatpak updates..."
-  flatpak update -y
+  flatpak update -y > flatpaklog.txt
+  flatcheck="$(grep "Nothing to do."  flatpaklog.txt)"
+  if [ "$flatcheck" = "Nothing to do." ]
+  then
+    echo "No flatpak updates."
+  else
+    echo "Flatpak apps updated!"
+  fi
+  rm flatpaklog.txt
   echo " "
-  echo "All Updates Complete!"
 fi
 
 # Exit
